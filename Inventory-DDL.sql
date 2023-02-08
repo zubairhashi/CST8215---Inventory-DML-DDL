844d8bc86281b8d57a30f68f9fd68008334e0121
zubairhashi <51759557+zubairhashi@users.noreply.github.com>
2023-02-08 14:43:06 -0500

    Add files via upload

---
+++ Inventory-DDL.sql
@@ -0,0 +1,96 @@
+﻿
+
+
+DROP VIEW IF EXISTS code;
+DROP MATERIALIZED VIEW IF EXISTS ID;
+
+
+DROP TABLE IF EXISTS Invoice_Line_T;
+DROP TABLE IF EXISTS Product_T;
+DROP TABLE IF EXISTS Invoice_T;
+DROP TABLE IF EXISTS Customer_T;
+DROP TABLE IF EXISTS city_t;
+DROP TABLE IF EXISTS COUNTRY_T;
+
+
+
+CREATE TABLE COUNTRY_T (
+
+ Cntry_Code char (3),
+ Cntry_Name varchar(30) NOT NULL, 
+ Cntry_Population bigint DEFAULT NULL,
+    
+ CONSTRAINT Cntry_Code_PK PRIMARY KEY (Cntry_Code)
+);
+
+
+
+CREATE TABLE city_t (
+ Cntry_Code char (3),
+ City_ID char (4),
+ City_Name varchar(30) NOT NULL,
+ City_Code char (3), 
+ City_Population   bigint DEFAULT NULL,
+ CONSTRAINT City_ID_PK PRIMARY KEY (City_ID),
+ CONSTRAINT Cntry_Code_FK FOREIGN KEY( Cntry_Code ) REFERENCES COUNTRY_T( Cntry_Code)
+);
+
+
+
+CREATE TABLE Customer_T (
+
+  Cust_Id         CHAR( 4 ),
+  Cust_Fname      VARCHAR( 30 ) NULL,
+  Cust_Lname      VARCHAR( 30 ) NOT NULL,
+  Cust_Phone      VARCHAR( 15 ) NOT NULL,
+  Cust_Address    VARCHAR( 20 ) NOT NULL,
+  Cust_City       VARCHAR( 15 ) NOT NULL,
+  Cust_Prov       CHAR( 2 ) NULL,
+  Cust_PostCode   CHAR( 6 ) NOT NULL,
+  Cust_Balance    DECIMAL( 9, 2 ),
+  Cust_Country    VARCHAR(30), 
+  CONSTRAINT Customer_PK PRIMARY KEY( Cust_Id ),
+  CONSTRAINT Cust_Country_FK FOREIGN KEY( Cust_Country ) REFERENCES COUNTRY_T( Cntry_Code)
+
+);
+
+CREATE TABLE Invoice_T (
+
+  Invoice_Number     CHAR( 6 ),
+  Cust_Id            CHAR( 4 ) NOT NULL,
+  Invoice_Date       DATE DEFAULT NOW(),
+  CONSTRAINT Invoice_PK PRIMARY KEY( Invoice_Number ),
+  CONSTRAINT Cust_ID_FK FOREIGN KEY( Cust_Id ) REFERENCES Customer_T( Cust_ID )
+);
+
+
+CREATE TABLE Product_T (
+  Cntry_Code       CHAR(3),
+  Prod_Code        CHAR( 5 ),
+  Prod_Description VARCHAR( 60 ) NOT NULL,
+  Prod_Indate      DATE NOT NULL DEFAULT NOW(),
+  Prod_QOH         INTEGER NOT NULL,
+  Prod_Min         INTEGER,
+  Prod_Price       DECIMAL( 5, 2 ) NOT NULL,
+  Prod_Discount    INTEGER,
+  Cntry_Origin     VARCHAR(30),
+  CONSTRAINT Product_PK PRIMARY KEY( Prod_Code ),
+  CONSTRAINT Cntry_Origin_FK FOREIGN KEY( Cntry_Origin) REFERENCES COUNTRY_T( Cntry_Code)
+
+);
+
+CREATE TABLE Invoice_Line_T (
+
+  Invoice_Number       CHAR( 6 ),
+  Invoice_Line         INTEGER,
+  Prod_Code            CHAR( 5 ) NOT NULL,
+  Line_Unit            INTEGER NOT NULL,
+  Line_Price           DECIMAL( 9, 2 ) NOT NULL,
+  CONSTRAINT Invoice_Line_PK  PRIMARY KEY( Invoice_Number, Invoice_Line ),
+  CONSTRAINT Invoice_Line_FK1 FOREIGN KEY( Invoice_Number ) REFERENCES Invoice_T( Invoice_Number ),
+  CONSTRAINT Invoice_Line_FK2 FOREIGN KEY( Prod_Code ) REFERENCES Product_T( Prod_Code )
+);
+
+
+
+
---
+++ Inventory-DML.sql
@@ -0,0 +1,98 @@
+﻿-- Filename: Inventory-DML.sql
+-- Description: script to populate Inventory Database
+-- CST 8215
+
+
+-- add at least 6 customers, 
+-- use atleast 3 different cities
+
+DELETE FROM Invoice_Line_T;
+DELETE FROM Product_T;
+DELETE FROM Invoice_T;
+DELETE FROM Customer_T;
+
+-- Add Customer data
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance )
+  VALUES( 'C001', 'Your first name', 'Your Last Name', '613-727-4723', '1385 Woodroffe Ave', 'Ottawa', 'ON', 'K2G1V8', 0 );
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance )
+  VALUES( 'C002', 'Your First Name', 'Your Last Name', '613-727-4723', '1385 Woodroffe Ave', 'Ottawa', 'ON', 'K2G1V8', 0 );
+
+UPDATE Customer_T 
+set Cust_Fname = 'Zubair',Cust_Lname = 'Hashi'
+WHERE Cust_ID = 'C002';
+
+  
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance )
+  VALUES( 'C003', 'Your First Name', 'Your Last Name', '613-727-4723', '1385 Woodroffe Ave', 'Ottawa', 'ON', 'K2G1V8', 0 );
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance )
+  VALUES( 'C004', 'Your First Name', 'Your Last Name', '613-727-4723', '854 Younge Street' , 'Toronto','ON', 'K2G1V8', 0 );
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance )
+  VALUES( 'C005', 'Your First Name', 'Your Last Name', '613-727-4723', '264 Main Street'   , 'Kitchener', 'ON', 'K2G1V8', 0 );
+INSERT INTO Customer_T( Cust_Id, Cust_Fname, Cust_Lname, Cust_Phone, Cust_Address, Cust_City, Cust_Prov, Cust_PostCode, Cust_Balance ) 
+  VALUES( 'C006', 'Your First Name', 'Your Last Name', '613-727-4723', '357 Rue Catherine' , 'Montreal', 'ON', 'K2G1V8', 0 );
+
+
+
+
+-- Add Invocie data
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23001', 'C001', '2011-02-15' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23002', 'C001', '2011-04-25' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23003', 'C004', '2011-06-12' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23004', 'C002', '2011-07-08' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23005', 'C005', '2011-08-24' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23006', 'C006', '2011-09-07' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23007', 'C006', '2010-12-28' );
+INSERT INTO Invoice_T( Invoice_Number, Cust_Id, Invoice_Date )
+  VALUES( 'I23008', 'C006', '2011-12-15' );
+
+
+-- Add Product data
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount )
+  VALUES( 'P2011', 'Compac Presario', '2011-02-14', 20, 5, 499.99, 0 );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2012', 'HP laptop', '2010-09-25', 40, 5, 529.99, 0 );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2013', 'Samsung LCD', '2010-02-15', 22, 8, 329.99, 0 );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2014', 'Brother Network All-In-One Laser Printer', '2010-10-10', 50, 10, 159.99, 0 );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2015', 'Western Digital External Hard drive', '2010-04-08', 30, 10, 149.99, NULL );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2016', 'Apple iPad 2 with Wi-Fi + 3G', '2011-02-23', 90, 200, 849.00, 0 );
+INSERT INTO Product_T( Prod_Code, Prod_Description, Prod_Indate, Prod_QOH, Prod_Min, Prod_Price, Prod_Discount ) 
+  VALUES( 'P2017', 'iPAD 2 Smart Cover', '2011-02-14', 70, 10, 45.00, 10 );
+
+
+-- Add Invoice Line Data
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+ VALUES( 'I23001', 1, 'P2011', 1, 650.75 ); 
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23001', 2, 'P2014', 3, 159.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23002', 1, 'P2012', 1, 529.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23003', 1, 'P2015', 3, 140.75 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23004', 1, 'P2014', 1, 159.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23005', 1, 'P2016', 1, 798.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23006', 1, 'P2011', 1, 499.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23007', 1, 'P2012', 1, 529.99 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23008', 1, 'P2016', 3, 689.00 );
+INSERT INTO Invoice_Line_T( Invoice_Number, Invoice_Line, Prod_Code, Line_Unit, Line_Price )
+  VALUES( 'I23008', 2, 'P2017', 3, 35.99 );
+
+-- eof: Inventory-DML.sql'
+
+
+
